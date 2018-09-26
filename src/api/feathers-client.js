eb31d391474e5b679eb145c4d2c012ca707fb0f0
flight9 <ealeaf@sohu.com>
2018-09-26 22:45:24 +0800

    update url for test on phone

--- src/api/feathers-client.js
+++ src/api/feathers-client.js
@@ -5,8 +5,8 @@ import socketio from 'feathers-socketio'
 import auth from 'feathers-authentication-client'
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:3030', {transports: ['websocket']}) // server for local test
-// const socket = io('http://tri.s1.natapp.cc', {transports: ['websocket']}) // server for wechat client
+// const socket = io('http://localhost:3030', {transports: ['websocket']}) // server from local test
+const socket = io('http://trisrv.s1.natapp.cc', {transports: ['websocket']}) // server from phone client
 
 const feathersClient = feathers()
   .configure(hooks())
